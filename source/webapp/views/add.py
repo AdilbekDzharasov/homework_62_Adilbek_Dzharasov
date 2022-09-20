7c8f792df44a80ec9cfb28b0ca230b816af2c971
Adilbek Dzharasov <dzharasov97@gmail.com>
2022-09-20 14:14:11 +0600

    Добавлен метод add_view отвечающий за добавления одной задачи

---
+++ source/webapp/views/add.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, redirect
+from webapp.models import Task
+
+
+def add_view(request):
+    if request.method == "GET":
+        return render(request, "add.html")
+    if request.POST.get('execute_at') == '':
+        execute = None
+    else:
+        execute = request.POST.get('execute_at')
+    task_data = {
+        'description': request.POST.get('description'),
+        'status': request.POST.get('status'),
+        'execute_at': execute
+    }
+    task = Task.objects.create(**task_data)
+    return redirect(f"/tasks/&pk={task.pk}")
