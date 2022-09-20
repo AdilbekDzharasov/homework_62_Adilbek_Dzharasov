aa30fa8c967d10fb8a37f0760417109cef9994f8
Adilbek Dzharasov <dzharasov97@gmail.com>
2022-09-20 14:41:06 +0600

    Добавлен путь для отображения одной задачи

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from webapp.views.base import home_view
-from webapp.views.add import add_view
+from webapp.views.add import add_view, detail_view
 
 urlpatterns = [
     path('', home_view),
-    path('tasks/add/', add_view)
+    path('tasks/add/', add_view),
+    path('tasks/', detail_view)
 ]
 
