41f0f496113224b59ceca21e57ca9bd9be4231c5
Adilbek Dzharasov <dzharasov97@gmail.com>
2022-09-20 13:19:31 +0600

    Добавил класс TaskAdmin для модели Task

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from webapp.models import Task
+
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['id', 'description', 'status', 'execute_at']
+    list_filter = ['id', 'description', 'status', 'execute_at']
+    search_fields = ['id', 'description']
+    fields = ['description', 'status', 'execute_at']
+    readonly_fields = ['id', 'execute_at']
+
+
+admin.site.register(Task, TaskAdmin)
+
