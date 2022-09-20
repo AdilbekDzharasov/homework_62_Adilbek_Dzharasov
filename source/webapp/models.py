495a8dfe2eadd664173ddd6d0b73d07e397b4f0f
Adilbek Dzharasov <dzharasov97@gmail.com>
2022-09-20 13:17:14 +0600

    Добавил модель Task

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    NEW = "NEW"
+    IN_THE_PROCESS = "IN_THE_PROCESS"
+    MADE = "MADE"
+
+    CHOICES = [
+        (NEW, 'Новая'),
+        (IN_THE_PROCESS, 'В процессе'),
+        (MADE, 'Сделано')
+    ]
+
+    description = models.TextField(max_length=3000, null=False, blank=False, verbose_name='Описание')
+    status = models.CharField(max_length=50, choices=CHOICES, verbose_name='Статус', default=NEW)
+    execute_at = models.DateField(verbose_name='Дата выполнения', null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.description} - {self.status} - {self.execute_at}"
+
