faf81a129ca6866f416439d628e50b47d138a6f9
Adilbek Dzharasov <dzharasov97@gmail.com>
2022-09-23 15:43:04 +0600

    Задал имена для всех url

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -3,8 +3,8 @@ from webapp.views.base import home_view
 from webapp.views.add import add_view, detail_view
 
 urlpatterns = [
-    path('', home_view),
-    path('tasks/add/', add_view),
-    path('tasks/', detail_view)
+    path('', home_view, name='task_home'),
+    path('tasks/add/', add_view, name='task_add'),
+    path('tasks/<int:pk>', detail_view, name='task_detail')
 ]
 
