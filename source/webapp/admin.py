1654f9b9f4db1e795fb744182a9bbf2830ecab01
Adilbek Dzharasov <dzharasov97@gmail.com>
2022-09-23 15:46:40 +0600

    Добавил новое поле-детальное описание в TaskAdmin

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -4,10 +4,10 @@ from webapp.models import Task
 
 
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ['id', 'description', 'status', 'execute_at']
-    list_filter = ['id', 'description', 'status', 'execute_at']
+    list_display = ['id', 'description', 'detail_description', 'status', 'execute_at']
+    list_filter = ['id', 'description', 'detail_description', 'status', 'execute_at']
     search_fields = ['id', 'description']
-    fields = ['description', 'status', 'execute_at']
+    fields = ['description', 'detail_description', 'status', 'execute_at']
     readonly_fields = ['id', 'execute_at']
 
 
