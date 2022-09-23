fc68895fdfb6ab0df6f2e3afbc49d2e621e9092b
Adilbek Dzharasov <dzharasov97@gmail.com>
2022-09-23 15:39:07 +0600

    Добавил в models.py поле-детальное описание и изменил choises

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -2,17 +2,16 @@ from django.db import models
 
 
 class Task(models.Model):
-    NEW = "NEW"
-    IN_THE_PROCESS = "IN_THE_PROCESS"
-    MADE = "MADE"
+    NEW = "Новая"
 
     CHOICES = [
         (NEW, 'Новая'),
-        (IN_THE_PROCESS, 'В процессе'),
-        (MADE, 'Сделано')
+        ("in_the_process", 'В процессе'),
+        ("made", 'Сделано')
     ]
 
-    description = models.TextField(max_length=3000, null=False, blank=False, verbose_name='Описание')
+    description = models.TextField(max_length=300, null=False, blank=False, verbose_name='Описание')
+    detail_description = models.TextField(max_length=3000, null=True, blank=True, verbose_name='Детальное описание')
     status = models.CharField(max_length=50, choices=CHOICES, verbose_name='Статус', default=NEW)
     execute_at = models.DateField(verbose_name='Дата выполнения', null=True, blank=True)
 
