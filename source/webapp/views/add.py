00bdbd3cde6a134f3fb712412f03f5baa9864a69
Adilbek Dzharasov <dzharasov97@gmail.com>
2022-09-30 00:34:10 +0600

    Изменена логика метода add_view, добавлены объект формы и валидация

--- source/webapp/views/add.py
+++ source/webapp/views/add.py
@@ -1,22 +1,24 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from webapp.models import Task
+from webapp.forms import TaskForm
 
 
 def add_view(request):
     if request.method == "GET":
-        return render(request, "add.html")
-    if request.POST.get('execute_at') == '':
-        execute = None
-    else:
-        execute = request.POST.get('execute_at')
-    task_data = {
-        'description': request.POST.get('description'),
-        'detail_description': request.POST.get('detail_description'),
-        'status': request.POST.get('status'),
-        'execute_at': execute
-    }
-    task = Task.objects.create(**task_data)
-    return redirect('task_detail', pk=task.pk)
+        form = TaskForm()
+        return render(request, "add.html", context={'form': form})
+    elif request.method == 'POST':
+        form = TaskForm(data=request.POST)
+        if form.is_valid():
+            task = Task.objects.create(
+                description=form.cleaned_data['description'],
+                detail_description=form.cleaned_data['detail_description'],
+                status=form.cleaned_data['status'],
+                execute_at=form.cleaned_data['execute_at']
+            )
+            return redirect('task_detail', pk=task.pk)
+        else:
+            return render(request, 'add.html', context={'form': form})
 
 
 def detail_view(request, pk):
