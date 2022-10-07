bc1c12f0004b55530a8caa5034fb6a2b95acaa33
Adilbek Dzharasov <dzharasov97@gmail.com>
2022-10-07 16:32:38 +0600

    Изменен класс формы для добавлкния и реадктирования задачи

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -1,16 +1,13 @@
 from django import forms
 from django.forms import widgets
+from webapp.models import Type, Status, Task
 
 
-class TaskForm(forms.Form):
-    CHOICES = [
-        ('new', 'New'),
-        ("in_the_process", 'In the process'),
-        ("made", 'Made')
-    ]
+class TaskForm(forms.ModelForm):
+    status = forms.ModelChoiceField(required=True, label='Summary', queryset=Status.objects.all())
+    type = forms.ModelChoiceField(required=True, label='Type', queryset=Type.objects.all())
 
-    description = forms.CharField(max_length=300, required=True, label='Description')
-    detail_description = forms.CharField(max_length=3000, required=False, label='Detail description', widget=widgets.Textarea)
-    status = forms.ChoiceField(required=True, label='Status', choices=CHOICES)
-    execute_at = forms.DateField(required=False, label='Execute at', widget=forms.widgets.DateInput(attrs={'type': 'date'}))
+    class Meta:
+        model = Task
+        fields = ('summary', 'description', 'status', 'type')
 
