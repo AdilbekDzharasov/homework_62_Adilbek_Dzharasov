c3741b1f95b14bcaedb5ba7167b3fd03a142b3d2
Adilbek Dzharasov <dzharasov97@gmail.com>
2022-10-07 16:33:16 +0600

    Изменены пути

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,15 +1,17 @@
 from django.urls import path
-from webapp.views.base import home_view
-from webapp.views.add import add_view, detail_view
-from webapp.views.delete_view import delete_task
-from webapp.views.update_view import update_task
+from webapp.views.base import HomeView
+from webapp.views.add import TaskAddView, TaskDetailView
+from webapp.views.delete_view import TaskDeleteView
+from webapp.views.update_view import TaskUpdateView
+
 
 
 urlpatterns = [
-    path('', home_view, name='task_home'),
-    path('tasks/add/', add_view, name='task_add'),
-    path('tasks/delete/<int:pk>', delete_task, name='task_delete'),
-    path('tasks/update/<int:pk>', update_task, name='task_update'),
-    path('tasks/<int:pk>', detail_view, name='task_detail')
+    path('', HomeView.as_view(), name='task_home'),
+    path('tasks/add/', TaskAddView.as_view(), name='task_add'),
+    path('tasks/delete/<int:pk>', TaskDeleteView.as_view(), name='task_delete'),
+    path('tasks/update/<int:pk>', TaskUpdateView.as_view(), name='task_update'),
+    path('tasks/<int:pk>', TaskDetailView.as_view(), name='task_detail')
 ]
+
 
