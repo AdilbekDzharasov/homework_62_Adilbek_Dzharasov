04cacc0c7a8e462477a526065f28917e50cb4d59
Adilbek Dzharasov <dzharasov97@gmail.com>
2022-10-07 16:29:18 +0600

    Изменен метод удаления задачи, на базе классовых представлений

--- source/webapp/views/delete_view.py
+++ source/webapp/views/delete_view.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from webapp.models import Task
+from django.views.generic import TemplateView
 
 
-def delete_task(request, pk):
-    task = get_object_or_404(Task, pk=pk)
-    if request.method == 'GET':
-        return render(request, 'delete.html', context={'task': task})
-    elif request.method == 'POST':
-        task.delete()
+class TaskDeleteView(TemplateView):
+    template_name = 'delete.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['task'] = get_object_or_404(Task, pk=kwargs['pk'])
+        return context
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        return render(request, "delete.html", context)
+
+    def post(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        context['task'].delete()
         return redirect('task_home')
 
