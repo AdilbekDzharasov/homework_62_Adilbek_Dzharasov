7879b677a1e5cc9dfd3ddba4e0ed2172974196e9
Adilbek Dzharasov <dzharasov97@gmail.com>
2022-10-07 16:29:47 +0600

    Изменен метод редактирования задачи, на базе классовых представлений

--- source/webapp/views/update_view.py
+++ source/webapp/views/update_view.py
@@ -1,27 +1,28 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import TemplateView
 from webapp.models import Task
 from webapp.forms import TaskForm
 
 
-def update_task(request, pk):
-    task = get_object_or_404(Task, pk=pk)
-    if request.method == 'GET':
-        form = TaskForm(initial={
-            'description': task.description,
-            'detail_description': task.detail_description,
-            'status': task.status,
-            'execute_at': task.execute_at
-        })
-        return render(request, 'update.html', context={'form': form, 'task': task})
-    elif request.method == 'POST':
-        form = TaskForm(data=request.POST)
+class TaskUpdateView(TemplateView):
+    template_name = 'update.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['task'] = get_object_or_404(Task, pk=kwargs['pk'])
+        return context
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        form = TaskForm(instance=context['task'])
+        context['form'] = form
+        return self.render_to_response(context)
+
+    def post(self, request, *args, **kwargs):
+        task = get_object_or_404(Task, pk=kwargs['pk'])
+        form = TaskForm(request.POST, instance=task)
         if form.is_valid():
-            task.description = form.cleaned_data['description']
-            task.detail_description = form.cleaned_data['detail_description']
-            task.status = form.cleaned_data['status']
-            task.execute_at = form.cleaned_data['execute_at']
-            task.save()
+            task = form.save()
             return redirect('task_detail', pk=task.pk)
-        else:
-            return render(request, 'update.html', context={'form': form, 'task': task})
+        return render(request, "update.html", context={'form': form})
 
