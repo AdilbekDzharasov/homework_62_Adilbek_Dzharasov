0c0a54fea17bfbf2450c01a78db062790d42a390
Adilbek Dzharasov <dzharasov97@gmail.com>
2022-10-11 15:17:13 +0600

    Добавил для widgets type, CheckboxSelectMultiple

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -5,9 +5,11 @@ from webapp.models import Type, Status, Task
 
 class TaskForm(forms.ModelForm):
     status = forms.ModelChoiceField(required=True, label='Summary', queryset=Status.objects.all())
-    type = forms.ModelChoiceField(required=True, label='Type', queryset=Type.objects.all())
 
     class Meta:
         model = Task
         fields = ('summary', 'description', 'status', 'type')
+        widgets = {
+            'type': widgets.CheckboxSelectMultiple
+        }
 
