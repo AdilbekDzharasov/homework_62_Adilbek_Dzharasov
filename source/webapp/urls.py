eda1d736e303d29e8ff20c9f76f9e94871036786
Adilbek Dzharasov <dzharasov97@gmail.com>
2022-10-14 16:47:40 +0600

    Добавлены и изменены пути

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from webapp.views.base import HomeView
-from webapp.views.add import TaskAddView, TaskDetailView
-from webapp.views.delete_view import TaskDeleteView
-from webapp.views.update_view import TaskUpdateView
-
+from webapp.tasks_views.base import HomeView
+from webapp.tasks_views.add import TaskAddView, TaskDetailView, ProjectTaskAddView
+from webapp.tasks_views.delete_view import TaskDeleteView
+from webapp.tasks_views.update_view import TaskUpdateView
+from webapp.projects_views.base_project import HomeProjectView
+from webapp.projects_views.add_project import ProjectDetailView, ProjectAddView
+from webapp.projects_views.delete_project import confirm_delete, ProjectDeleteView
 
 
 urlpatterns = [
@@ -11,7 +13,12 @@ urlpatterns = [
     path('tasks/add/', TaskAddView.as_view(), name='task_add'),
     path('tasks/delete/<int:pk>', TaskDeleteView.as_view(), name='task_delete'),
     path('tasks/update/<int:pk>', TaskUpdateView.as_view(), name='task_update'),
-    path('tasks/<int:pk>', TaskDetailView.as_view(), name='task_detail')
+    path('tasks/<int:pk>', TaskDetailView.as_view(), name='task_detail'),
+    path('projects', HomeProjectView.as_view(), name='project_home'),
+    path('projects/<int:pk>', ProjectDetailView.as_view(), name='project_detail'),
+    path('projects/add/', ProjectAddView.as_view(), name='project_add'),
+    path('projects/<int:pk>/tasks/add/', ProjectTaskAddView.as_view(), name='project_task_create'),
+    path('projects/confirm_delete/<int:pk>', confirm_delete, name='project_delete'),
+    path('projects/delete/<int:pk>', ProjectDeleteView.as_view(), name='project_all_delete')
 ]
-
 
