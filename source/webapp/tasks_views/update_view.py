bfd6ce6f6d3e67ae153ba2093a331ac5b0d92eb6
Adilbek Dzharasov <dzharasov97@gmail.com>
2022-10-14 15:56:03 +0600

    Изменен методы добавления и детального просмотра задачи с помощью CreateView и DetailView

--- source/webapp/tasks_views/update_view.py
+++ source/webapp/tasks_views/update_view.py
@@ -1,30 +1,15 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from django.views.generic import TemplateView
+from django.urls import reverse
+from django.views.generic import UpdateView
 from webapp.models import Task
 from webapp.forms import TaskForm
 
 
-class TaskUpdateView(TemplateView):
-    template_name = 'update.html'
+class TaskUpdateView(UpdateView):
+    template_name = 'tasks/update.html'
+    form_class = TaskForm
+    model = Task
+    context_object_name = 'task'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['task'] = get_object_or_404(Task, pk=kwargs['pk'])
-        return context
-
-    def get(self, request, *args, **kwargs):
-        context = self.get_context_data(**kwargs)
-        form = TaskForm(instance=context['task'])
-        context['form'] = form
-        return self.render_to_response(context)
-
-    def post(self, request, *args, **kwargs):
-        task = get_object_or_404(Task, pk=kwargs['pk'])
-        form = TaskForm(request.POST, instance=task)
-        if form.is_valid():
-            task = form.save()
-            return redirect('task_detail', pk=task.pk)
-        context = self.get_context_data(**kwargs)
-        context['form'] = context
-        return self.render_to_response(context)
+    def get_success_url(self):
+        return reverse('task_detail', kwargs={'pk': self.object.pk})
 
