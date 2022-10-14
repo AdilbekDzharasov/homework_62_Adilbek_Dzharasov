863742c13e1c1085744790c6e628977d4f4e506e
Adilbek Dzharasov <dzharasov97@gmail.com>
2022-10-14 14:36:12 +0600

    Добавил модельную форму для проекта и форму для поиска

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.forms import widgets
 from webapp.models import Type, Status, Task
+from webapp.models.projects import Project
+from webapp.widgets import DatePickerInput
 
 
 class TaskForm(forms.ModelForm):
@@ -12,4 +14,17 @@ class TaskForm(forms.ModelForm):
         widgets = {
             'type': widgets.CheckboxSelectMultiple
         }
+
+
+class ProjectForm(forms.ModelForm):
+    beginning_date = forms.DateField(required=True, label='Beginning date', widget=DatePickerInput)
+    expiration_date = forms.DateField(required=False, label='Expiration date', widget=DatePickerInput)
+
+    class Meta:
+        model = Project
+        fields = ('title', 'description', 'beginning_date', 'expiration_date')
+
+
+class SearchForm(forms.Form):
+    search = forms.CharField(max_length=100, required=False, label="Search")
 
