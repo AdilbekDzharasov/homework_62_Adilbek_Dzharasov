19e7c6d9809afc3195130893d566f0d00907ba45
Adilbek Dzharasov <dzharasov97@gmail.com>
2022-10-21 14:11:02 +0600

    Внес корректировки в LoginForm

--- source/accounts/forms.py
+++ source/accounts/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 
 class LoginForm(forms.Form):
-    username = forms.CharField(required=True, label='Логин')
-    password = forms.CharField(required=True, label='Пароль', widget=forms.PasswordInput)
+    username = forms.CharField(required=True, label='Login')
+    password = forms.CharField(required=True, label='Password', widget=forms.PasswordInput)
     next = forms.CharField(required=False, widget=forms.HiddenInput)
 
